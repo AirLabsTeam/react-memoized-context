080e0205d71830367349d1b360d27cecc9ed1f69
Dan Wilt <dwilt4rville@gmail.com>
2022-08-08 10:27:03 -0400

    fixed example exports and TS issues

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-import { useMemoizedContextProvider, defaultMemoizedContextValue } from './hooks/useMemoizedContextProvider';
+import {
+  useMemoizedContextProvider,
+  defaultMemoizedContextValue,
+  MemoizedContextAction,
+  MemoizedContextType,
+} from './hooks/useMemoizedContextProvider';
 import { useMemoizedContextSelector } from './hooks/useMemoizedContextSelector';
 export { useMemoizedContextProvider, useMemoizedContextSelector, defaultMemoizedContextValue };
+export type { MemoizedContextAction, MemoizedContextType };
